0e592ead9585ae475bc84598d27af5021528575d
Matt Hidinger <matt.hidinger@gmail.com>
2011-11-27 23:57:03 -0600

    - Moved Web back into the core project and tested using the NuGet package in a WinForms project and working well - Added Nuspecs for OpenXml and AbcPdf

---
+++ nuget/PackAndPublish.bat
@@ -0,0 +1,3 @@
+nuget pack ..\src\Doddle.Reporting\Doddle.Reporting.csproj -Build
+nuget pack ..\src\Doddle.Reporting.AbcPdf\Doddle.Reporting.AbcPdf.csproj -Build
+nuget pack ..\src\Doddle.Reporting.OpenXml\Doddle.Reporting.OpenXml.csproj -Build
