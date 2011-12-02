fa5f2d56f89feeee3dfc9247d37d9b1bb00b07a0
mhidinger <mhidinger@localhost>
2011-12-02 11:30:41 -0600

    - Added ability to manually specify the ReportWriter from a ReportResult per request http://doddlereport.codeplex.com/discussions/262362

--- src/Doddle.Reporting/Web/ReportResult.cs
+++ src/Doddle.Reporting/Web/ReportResult.cs
@@ -8,30 +8,60 @@ namespace Doddle.Reporting.Web
     public class ReportResult : ActionResult
     {
         private readonly Report _report;
+        private IReportWriter _writer;
+        private readonly string _contentType;
 
         public ReportResult(Report report)
         {
             _report = report;
         }
 
+        public ReportResult(Report report, IReportWriter writer, string contentType = null)
+        {
+            _report = report;
+            _writer = writer;
+            _contentType = contentType;
+        }
+
+
         public override void ExecuteResult(ControllerContext context)
+        {
+            var response = context.HttpContext.Response;
+
+            if(_writer == null)
+            {
+                var writerConfig = GetWriterFromExtension(context);
+                response.ContentType = writerConfig.ContentType;
+                _writer = writerConfig.LoadWriter();
+            }
+            else
+            {
+                response.ContentType = _contentType;               
+            }
+
+            _writer.WriteReport(_report, response.OutputStream);
+        }
+
+        private static WriterElement GetWriterFromExtension(ControllerContext context)
         {
             string defaultExtension = Config.Report.Writers[Config.Report.DefaultWriter].FileExtension;
 
+            // attempt to get the report format from the extension on the URL (ex. "/action/controller.pdf" yields ".pdf")
             string extension = Path.GetExtension(context.HttpContext.Request.RawUrl);
-            if(string.IsNullOrEmpty(extension))
+
+            if (string.IsNullOrEmpty(extension))
             {
                 extension = defaultExtension;
             }
 
             var writerConfig = Config.Report.Writers.GetWriterConfigurationForFileExtension(extension);
-            if(writerConfig == null)
-                throw new InvalidOperationException(string.Format("Unable to locate a report writer for the extension '{0}'. Did you add this fileExtension to the web.config for DoddleReport?", extension));
+            if (writerConfig == null)
+                throw new InvalidOperationException(
+                    string.Format(
+                        "Unable to locate a report writer for the extension '{0}'. Did you add this fileExtension to the web.config for DoddleReport?",
+                        extension));
 
-            var writer = writerConfig.LoadWriter();
-            var response = context.HttpContext.Response;
-            response.ContentType = writerConfig.ContentType;
-            writer.WriteReport(_report, response.OutputStream);
+            return writerConfig;
         }
     }
 }
