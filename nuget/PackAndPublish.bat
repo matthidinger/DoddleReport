5960fcd291615b4c65e2abe4d6c5394f917c2d2d
mhidinger <mhidinger@localhost>
2011-12-02 11:55:01 -0600

    - Added a DoddleReport.Dynamic NuGet package to get the DynamicReportSource file added to a project - Fixed the StreamWriters to Flush before the method exits

--- nuget/PackAndPublish.bat
+++ nuget/PackAndPublish.bat
@@ -1,4 +1,6 @@
 nuget pack ..\src\Doddle.Reporting\Doddle.Reporting.csproj -Build
+nuget pack Doddle.Reporting.Dynamic.nuspec
 nuget pack ..\src\Doddle.Reporting.AbcPdf\Doddle.Reporting.AbcPdf.csproj -Build
 nuget pack ..\src\Doddle.Reporting.OpenXml\Doddle.Reporting.OpenXml.csproj -Build
 
+pause
