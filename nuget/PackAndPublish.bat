238c925c6dad8239b1090262bd477702b8241f9e
Matt Hidinger <matt.hidinger@gmail.com>
2011-12-02 10:22:44 -0600

    - Separating v3.5 from v4 to add Dynamic reporting features.

--- nuget/PackAndPublish.bat
+++ nuget/PackAndPublish.bat
@@ -1,3 +1,4 @@
 nuget pack ..\src\Doddle.Reporting\Doddle.Reporting.csproj -Build
 nuget pack ..\src\Doddle.Reporting.AbcPdf\Doddle.Reporting.AbcPdf.csproj -Build
 nuget pack ..\src\Doddle.Reporting.OpenXml\Doddle.Reporting.OpenXml.csproj -Build
+
---
+++ src/Doddle.Reporting.v4/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Doddle.Reporting.v4
+{
+    public class Class1
+    {
+    }
+}
