955feaa12b17644d5cf14ad33fb51802d849458c
Matt Hidinger <matt.hidinger@gmail.com>
2016-04-09 17:12:06 -0700

    removed the strong naming of the assemblies; updated the nuget packaging to include debug symbol generation

--- src/DoddleReport/Properties/AssemblyInfo.cs
+++ src/DoddleReport/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Matt Hidinger")]
 [assembly: AssemblyProduct("DoddleReport")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
